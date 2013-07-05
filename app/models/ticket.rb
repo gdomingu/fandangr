dedb926c38d026a5661fe1bf2b6467bc45994620
Gabe <gabedominguez@outlook.com>
2013-07-05 15:06:57 -0700

    added spec for validates price, screening_id and seat_num

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,5 @@
 class Ticket < ActiveRecord::Base
   attr_accessible :price, :screening_id, :seat_num, :user_id
 
-
+  validates :price, :screening_id, :seat_num, :presence => true
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -3,6 +3,15 @@ require 'spec_helper'
 describe Ticket do
   it "is created" do
     ticket = Ticket.new(:price => 12.99, :screening_id => 1, :seat_num => 34, :user_id => 1)
+    ticket.save
+
+    tickets = Ticket.all
+    expect(tickets).to include(ticket)
+  end
+
+  it "tries to create one without price, screening id, and seat" do
+    ticket = Ticket.new
+    expect(ticket.valid?).to be_false
   end
 
 end
